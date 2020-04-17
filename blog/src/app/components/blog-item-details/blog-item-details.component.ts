15294edfdab5d874e9087896b110b6610b8ea69d
Michał Sitko <cprl.28342@gmail.com>
2020-04-17 11:10:09 +0200

    pozostale zadania z lab7

--- blog/src/app/components/blog-item-details/blog-item-details.component.ts
+++ blog/src/app/components/blog-item-details/blog-item-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../../services/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'blog-item-details',
@@ -6,12 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-item-details.component.css']
 })
 export class BlogItemDetailsComponent implements OnInit {
-  image = 'http://osnews.pl/wp-content/uploads/2016/06/it-grafika.jpg';
-  text = 'Tytuł';
+  public image = '';
+  public text: string;
+  public id: number;
 
-  constructor() { }
+  constructor(private dataService: DataService,private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let id: string;
+    this.activatedRoute.paramMap.subscribe(params =>{
+      id = params.get("id");
+    });
+    if(id){
+      this.dataService.getById(id).subscribe(response =>{
+        this.image = response['image'];
+        this.text = response['text'];
+      });
+    } else {
+      this.id = 1;
+    }
   }
 
 }
--- blog/src/app/services/data.service.ts
+++ blog/src/app/services/data.service.ts
@@ -5,7 +5,7 @@ import {HttpClient} from '@angular/common/http';
 @Injectable()
 export class DataService {
 
-  private url = 'https://blogtai.herokuapp.com;
+  private url = 'https://blogtai.herokuapp.com';
 
   constructor(private http: HttpClient) { }
 
@@ -17,4 +17,7 @@ export class DataService {
     return this.http.get(this.url+'/api/posts/'+id);
   }
 
+  getByText(data: any){
+    return this.http.post(this.url+'/api/posts/',data);
+  }
 }
